455986053f479e25d2c4dfdd57fbbd9eccd9b19c
Scottie Crump <sncrump29@gmail.com>
2021-02-18 22:33:25 -0500

    refactor checkout flow to Cypress Testing Library

--- ch_07/start_cypress/cypress/e2e/checkOutFlow.spec.js
+++ ch_07/start_cypress/cypress/e2e/checkOutFlow.spec.js
@@ -5,40 +5,44 @@ describe('Checkout Flow', () => {
     cy.visit('/')
 
     // Enter Address
-    cy.get('input[name="firstName"]').type(user.firstName)
-    cy.get('input[name="lastName"]').type(user.lastName)
-    cy.get('input[name="address1"]').type(user.address1)
-    cy.get('input[name="city"]').type(user.city)
-    cy.get('input[name="state"]').type(user.state)
-    cy.get('input[name="zipCode"]').type(user.zipCode)
-    cy.get('input[name="country"]').type(user.country)
-    cy.contains(/next/i).click()
+    cy.findByRole('textbox', { name: /first name/i }).type(user.firstName)
+    cy.findByRole('textbox', { name: /last name/i }).type(user.lastName)
+    cy.findByRole('textbox', { name: /address line 1/i }).type(user.address1)
+    cy.findByRole('textbox', { name: /city/i }).type(user.city)
+    cy.findByRole('textbox', { name: /state/i }).type(user.state)
+    cy.findByRole('textbox', { name: /postal code/i }).type(user.zipCode)
+    cy.findByRole('textbox', { name: /country/i }).type(user.country)
+    cy.findByText(/next/i).click()
 
     // Enter Payment Info
-    cy.get('input[name="cardType"]').type(user.cardType)
-    cy.get('input[name="cardHolder"]').type(user.cardHolder)
-    cy.get('input[name="cardNumber"]').type(user.cardNumber)
-    cy.get('input[name="expiryDate"]').type(user.expiryDate)
-    cy.get('input[name="cardCvv"]').type(user.cardCvv)
-    cy.contains(/next/i).click()
+    cy.findByRole('textbox', { name: /card type/i }).type(user.cardType)
+    cy.findByRole('textbox', { name: /name on card/i }).type(user.cardHolder)
+    cy.findByRole('textbox', { name: /card number/i }).type(user.cardNumber)
+    cy.findByRole('textbox', { name: /expiration date/i }).type(user.expiryDate)
+    cy.findByRole('textbox', { name: /cvv/i }).type(user.cardCvv)
+    cy.findByText(/next/i).click()
 
     // Verify Shipping Details
-    cy.contains(`${user.firstName} ${user.lastName}`).should('be.visible')
-    cy.contains(user.address1).should('be.visible')
-    cy.contains(`${user.city}, ${user.state} ${user.zipCode}`).should(
+    cy.findByText(`${user.firstName} ${user.lastName}`).should('be.visible')
+    cy.findByText(user.address1).should('be.visible')
+    cy.findByText(`${user.city}, ${user.state} ${user.zipCode}`).should(
       'be.visible'
     )
-    cy.contains(user.country).should('be.visible')
+    cy.findByText(user.country).should('be.visible')
 
     // Verify Payment Details
-    cy.contains(user.cardType).should('be.visible')
-    cy.contains(user.cardHolder).should('be.visible')
-    cy.contains(user.cardNumber).should('be.visible')
-    cy.contains(user.expiryDate).should('be.visible')
-    cy.contains(/place order/i).click()
+    cy.findByText(user.cardType).should('be.visible')
+    cy.findByText(user.cardHolder).should('be.visible')
+    cy.findByText(user.cardNumber).should('be.visible')
+    cy.findByText(user.expiryDate).should('be.visible')
+    cy.findByText(/place order/i).click()
 
     // Verify Order Submitted
-    cy.contains(/thank you for your order/i).should('be.visible')
-    cy.contains(/your order number is #2001539/i).should('be.visible')
+    cy.findByRole('heading', { name: /thank you for your order/i }).should(
+      'be.visible'
+    )
+    cy.findByRole('heading', { name: /your order number is #2001539/i }).should(
+      'be.visible'
+    )
   })
 })
