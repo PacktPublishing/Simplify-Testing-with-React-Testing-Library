747d45673e1540a196287964fadd9d648d872f12
Scottie Crump <sncrump29@gmail.com>
2021-03-22 21:26:12 -0400

    update dependencies

--- ch_03/src/Register.test.js
+++ ch_03/src/Register.test.js
@@ -5,20 +5,20 @@ import Register from './Register'
 describe('Registration', () => {
   test('given submitted form, invokes handleRegister', () => {
     const mockHandleRegister = jest.fn()
-    const mockValues = {
+    const mockUser = {
       email: 'john@mail.com',
       password: '123'
     }
 
     render(<Register handleRegister={mockHandleRegister} />)
 
-    user.type(screen.getByLabelText('Email Address'), mockValues.email)
-    user.type(screen.getByLabelText('Create Password'), mockValues.password)
+    user.type(screen.getByLabelText('Email Address'), mockUser.email)
+    user.type(screen.getByLabelText('Create Password'), mockUser.password)
     user.click(screen.getByRole('button', { name: /submit/i }))
 
     expect(mockHandleRegister).toHaveBeenCalledWith({
-      email: mockValues.email,
-      password: mockValues.password
+      email: mockUser.email,
+      password: mockUser.password
     })
     expect(mockHandleRegister).toHaveBeenCalledTimes(1)
   })
