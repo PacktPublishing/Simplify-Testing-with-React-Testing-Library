5628aa191bebe0b6eeb064494ebd80783107dfb4
Scottie Crump <sncrump29@gmail.com>
2021-04-07 01:21:42 -0400

    chapter 1 propTypes additions

--- Chapter01/src/Profile.js
+++ Chapter01/src/Profile.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 export default class Profile extends Component {
@@ -9,24 +10,31 @@ export default class Profile extends Component {
 
   render() {
     return (
-      <div className="card" style={{ width: '18rem' }}>
+      <div className='card' style={{ width: '18rem' }}>
         <img
-          className="card-img-top"
-          src="http://fakeimg.pl/286x180?font=lobster"
-          alt="Card cap"
+          className='card-img-top'
+          src='http://fakeimg.pl/286x180?font=lobster'
+          alt='Card cap'
         />
-        <div className="card-body">
-          <h5 className="card-title">{this.props.name}</h5>
-          <p className="card-subtitle mb-2 text-muted">{this.props.title}</p>
-          <button onClick={this.setDetails} className="btn btn-primary">
+        <div className='card-body'>
+          <h5 className='card-title'>{this.props.name}</h5>
+          <p className='card-subtitle mb-2 text-muted'>{this.props.title}</p>
+          <button onClick={this.setDetails} className='btn btn-primary'>
             {this.state.showDetails ? 'Hide Details' : 'Display Details'}
           </button>
 
           {this.state.showDetails ? (
-            <p className="card-text details">{this.props.details}</p>
+            <p className='card-text details'>{this.props.details}</p>
           ) : null}
         </div>
       </div>
     );
   }
 }
+
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  details: PropTypes.string.isRequired,
+};
